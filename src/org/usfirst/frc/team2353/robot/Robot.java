705199b29882b10957127b351783d1fee32c9ff8
Byte11 <shirmak11@gmail.com>
2017-02-08 16:25:37 -0600

    Revert "Removed Comment"
    This reverts commit e3a86e73b4e4c7654a13bb25434446716fe96e6a.

--- src/org/usfirst/frc/team2353/robot/Robot.java
+++ src/org/usfirst/frc/team2353/robot/Robot.java
@@ -53,6 +53,17 @@ public class Robot extends IterativeRobot {
 		Scheduler.getInstance().run();
 	}
 
+	/**
+	 * This autonomous (along with the chooser code above) shows how to select
+	 * between different autonomous modes using the dashboard. The sendable
+	 * chooser code works with the Java SmartDashboard. If you prefer the
+	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+	 * getString code to get the auto name from the text box below the Gyro
+	 *
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons
+	 * to the switch structure below with additional strings & commands.
+	 */
 	@Override
 	public void autonomousInit() {
 		autonomousCommand = chooser.getSelected();
