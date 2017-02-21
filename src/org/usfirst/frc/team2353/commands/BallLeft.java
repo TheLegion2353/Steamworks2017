6425ac8f7aff463eb1b2f818e0c70b541ce263c5
Byte11 <shirmak11@gmail.com>
2017-02-20 20:01:15 -0600

    Modified the drive speed on the Autonomous for the ball collection.

--- src/org/usfirst/frc/team2353/commands/BallLeft.java
+++ src/org/usfirst/frc/team2353/commands/BallLeft.java
@@ -8,12 +8,12 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class BallLeft extends CommandGroup {
 
     public BallLeft() {
-    	addSequential(new TimedDrive(0, 5, 0, 0, 0.25)); //Move Forward
-    	addSequential(new TimedDrive(0, 0, 5, 0, 0.2)); //Turn 45 Degrees
-    	addSequential(new TimedDrive(0, -5, 0, 0, 0.4)); //Move backwards to the boiler
+    	addSequential(new TimedDrive(0, 0.6, 0, 0, 0.25)); //Move Forward
+    	addSequential(new TimedDrive(0, 0, 0.5, 0, 0.2)); //Turn 45 Degrees
+    	addSequential(new TimedDrive(0, -0.6, 0, 0, 0.4)); //Move backwards to the boiler
     	addSequential(new DumpBalls(0.4)); //Dump balls
-    	addSequential(new TimedDrive(0, 5, 0, 0, 0.4)); //Move Forward
-    	addSequential(new TimedDrive(0, 0, -5, 0, 0.4)); //Turn 45 degrees to Breach
-    	addSequential(new TimedDrive(0, 5, 0, 0, 1)); //Move forward to breach
+    	addSequential(new TimedDrive(0, 0.6, 0, 0, 0.4)); //Move Forward
+    	addSequential(new TimedDrive(0, 0, -0.5, 0, 0.2)); //Turn 45 degrees to Breach
+    	addSequential(new TimedDrive(0, 0.6, 0, 0, 1)); //Move forward to breach
     }
 }
